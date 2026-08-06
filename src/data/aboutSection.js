d708ba7468072f478947f6aaad61369c65f14333
Vladimir Santillana <vladisant@gmail.com>
2026-08-06 15:52:35 +0200

    a

--- src/data/aboutSection.js
+++ src/data/aboutSection.js
@@ -6,13 +6,3 @@ export const ABOUT_SECTION_MUSIC = {
   embedUrl:
     'https://bandcamp.com/EmbeddedPlayer/album=3945260847/size=small/bgcol=333333/linkcol=e32c14/track=4245757193/transparent=true/',
 }
-
-// export const ABOUT_SECTION_MUSIC = {
-//   title: 'LISTEN ON BANDCAMP',
-//   bandcampUrl:
-//     'https://runstate-tapes.com/album/a-varning-from-montreal-compilation-vol-1-v-a-2018',
-//   fallbackText: 'Shove Their System Up Their Ass LP by Languid',
-//   // Swap this URL with any Bandcamp embed URL from "Share/Embed".
-//   embedUrl:
-//     'https://bandcamp.com/EmbeddedPlayer/album=2502953782/size=small/bgcol=333333/linkcol=e32c14/artwork=none/track=792503520/transparent=true/',
-// }
